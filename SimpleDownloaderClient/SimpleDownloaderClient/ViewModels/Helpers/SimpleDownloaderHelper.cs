cf150716c0c6a5638a8c5bdda1f0bd66eaa80aa5
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-02-22 17:18:54 +0100

    Refactor usings

--- SimpleDownloaderClient/SimpleDownloaderClient/ViewModels/Helpers/SimpleDownloaderHelper.cs
+++ SimpleDownloaderClient/SimpleDownloaderClient/ViewModels/Helpers/SimpleDownloaderHelper.cs
@@ -11,35 +11,28 @@
     {
         public static async Task<List<Item>> GetItemsAsync(string server)
         {
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync($"{server}/downloads");
-                response.EnsureSuccessStatusCode();
-                var json = await response.Content.ReadAsStringAsync();
-                return JsonSerializer.Deserialize<Data>(json).Items;
-            }
+            using var client = new HttpClient();
+            var response = await client.GetAsync($"{server}/downloads");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Data>(json).Items;
         }
 
         public static async Task<bool> DownloadItemAsync(string server, string url)
         {
-            using (var client = new HttpClient())
-            {
-                var content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("url", url) });
-                var response = await client.PostAsync($"{server}/download", content);
-                return response.IsSuccessStatusCode;
-            }
+            using var client = new HttpClient();
+            var content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("url", url) });
+            var response = await client.PostAsync($"{server}/download", content);
+            return response.IsSuccessStatusCode;
         }
 
         public static async Task<bool> DeleteItemAsync(string server, string actions)
         {
             var match = Regex.Match(actions, @"delete\/[^']*");
             var url = $"{server}/{match.Groups[0].Value}";
-
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync(url);
-                return response.IsSuccessStatusCode;
-            }
+            using var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            return response.IsSuccessStatusCode;
         }
     }
 }
